ae10d1752eb9d8cc3156ec9728d5a8316ec3f9bc
bernard24 <bernard24@gmail.com>
2016-11-07 23:53:15 +0000

    Replacing load_everything with the right version.

--- people_learning/load_everything.lua
+++ people_learning/load_everything.lua
@@ -1,16 +1,5 @@
 require 'nn'
 require 'cunn'
---
---require 'cutorch'
---if we_have_cudnn==1 then
-	require 'cudnn'
-	cudnn.benchmark = true
-	cudnn.fastest = true
-	--cudnn.verbose = true
---else
---	require 'cunn'
---end
---require 'inn'
 
 torch.setdefaulttensortype('torch.FloatTensor')
 
@@ -23,16 +12,7 @@ Fcn8 = require 'Fcn8'
 require 'optim'
 require 'misc'
 require 'create_lstm_protos'
-require 'IoU3Criterion'
-require 'IoU5Criterion'
-require 'IoUMultiClassCriterion'
-require 'MatchCriterion2'
-
-require 'MatchClassesCriterion'
-require 'create_classes_lstm_protos'
-require 'create_classes_lstm_protos_2'
-
-require 'get_instance'
+require 'MatchCriterion'
 
 function getParameter(nngraph_model, name)
     local params
