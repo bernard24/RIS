59410340953e9504f516c3895ef712859ae5d56e
Bernardino Romera-Paredes <bernard24@gmail.com>
2016-11-09 10:26:17 +0000

    Update format_instance_labels.m

--- people_learning/data_utils/format_instance_labels.m
+++ people_learning/data_utils/format_instance_labels.m
@@ -12,9 +12,6 @@ while ischar(tline)
     if elements(1)==0
         elements = elements(2:end);
     end
-    if elements(end)==255
-        elements = elements(1:end);
-    end
     class_elements = [];
     for j=1:length(elements)
         class_elements = [class_elements, class_im(find(object_im==elements(j), 1))];
